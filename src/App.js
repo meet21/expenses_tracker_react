2652233fb131eaa5572531886743420f14d7ba46
Ecell-official <meet21bhalgamiya@gmail.com>
2021-03-16 13:12:07 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./app.css";
+import { Provider } from "react-redux";
+import { applyMiddleware } from "redux";
 
-function App() {
+import reducer from "./reducer";
+import { createStore } from "redux";
+
+import NavBar from "./components/Nav";
+import { Typography, Divider } from "@material-ui/core";
+
+import AuthRoute from "./components/AuthRoute";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/Login";
+
+import { appMiddleware } from "./middlewares/app";
+import { apiMiddleware } from "./middlewares/core";
+import MyAccount from "./pages/MyAccount";
+
+const createStoreWithMiddleware = applyMiddleware(
+  appMiddleware,
+  apiMiddleware
+)(createStore);
+
+const store = createStoreWithMiddleware(reducer);
+
+const IndexPage = () => (
+  <>
+    <Typography variant="h3">Welcome to the App</Typography>
+    <Divider style={{ marginTop: 10, marginBottom: 10 }} />
+    <Typography variant="h6">Feel free to take a look around</Typography>
+  </>
+);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <NavBar />
+        <div className="container">
+          <Switch>
+            <AuthRoute path="/home" render={HomePage} type="private" />
+            <AuthRoute path="/login" type="guest">
+              <LoginPage />
+            </AuthRoute>
+            <AuthRoute path="/my-account" type="private">
+              <MyAccount />
+            </AuthRoute>
+            <Route path="/" render={IndexPage} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
-
-export default App;
